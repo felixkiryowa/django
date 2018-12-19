5c379fdef77f6c822ad2eb240ff1864c2c0bee68
felixkiryowa <franciskiryowa68@gmail.com>
2018-12-18 22:22:20 -0800

    Add logic to work with database tables

--- posts/views.py
+++ posts/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Posts
 
 # Create your views here.
 
 
+
 def index(request):
-    return render(request, 'posts/index.html')
+    posts = Posts.objects.all()[:10]
+    template_data = {
+        'title':'Latest Posts',
+        'posts':posts
+    }
+    return render(request, 'posts/index.html', template_data)
+
+def details(request, id):
+    post = Posts.objects.get(id=id)
+    template_data = {
+        'post':post
+    } 
+    return render(request, 'posts/details.html', template_data)
+
