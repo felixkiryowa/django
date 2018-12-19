4cc4a661e23b0e0f77a63a01350b10b82304802f
felixkiryowa <franciskiryowa68@gmail.com>
2018-12-19 00:43:53 -0800

    Fix issues with displaying a single order

--- posts/urls.py
+++ posts/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('/details/<int:id>', views.details, name='details')
+    path('details/<int:id>', views.details, name='details')
 ]
